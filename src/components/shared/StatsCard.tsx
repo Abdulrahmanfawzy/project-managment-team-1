3b7a2e1fd63658d3a77793c91e332accf0af0a82
MahmoudRafaat20 <mahmoudrafaatmahmoud3@gmail.com>
2026-06-24 19:21:38 +0300

    added dashboard

--- src/components/shared/StatsCard.tsx
+++ src/components/shared/StatsCard.tsx
@@ -1,13 +1,14 @@
+
 import { ArrowUpRight } from 'lucide-react'
 
 
-export default function StatsCard({ title, value, subValue, icon }) {
+export default function StatsCard({ title, value, subValue, icon, progressValue, color,backgroundColor ,iconColor}) {
 
   return (
     <div className="p-4 shadow-sm text-black w-80 space-y-2 rounded-sm">
       <div className="flex justify-between items-center">
         <h2 className="text-lg font-semibold">{title}</h2>
-        {icon}
+        <span className={`${backgroundColor} ` } style={{color:`${iconColor}`}}>{icon}</span>
         {/* <icon className="text-amber-500 bg-amber-100 rounded-full w-10 h-8" /> */}
       </div>
       <div className="flex space-x-2 items-center">
@@ -15,10 +16,11 @@ export default function StatsCard({ title, value, subValue, icon }) {
         <ArrowUpRight size={18} strokeWidth={0.75} className="text-green-800" />
         <p className="text-sm text-gray-500">{subValue}</p>
       </div>
-      <h2 className="text-end">{value}</h2>
-      <div className="w-full bg-amber-100 rounded-full ">
-        <div className="bg-amber-500  rounded-full w-1/6 h-2.5"></div>
+      <h2 className="text-end">{progressValue}</h2>
+      <div className="w-full bg-gray-200 rounded-full h-4">
+        <div className={`${color} h-4 rounded-full w-1/2`} ></div>
       </div>
     </div>
   )
 }
+
--- src/features/Dashboard/DashboardPage.tsx
+++ src/features/Dashboard/DashboardPage.tsx
@@ -1,8 +1,11 @@
 import {
   ArrowUpRight,
+  Backpack,
   ClipboardCheck,
   ClipboardClock,
   ClipboardList,
+  Download,
+  FileText,
   Gauge,
 } from "lucide-react";
 import { TrendingUp } from "lucide-react";
@@ -22,7 +25,11 @@ import {
   type ChartConfig,
 } from "@/components/ui/chart";
 import StatsCard from "@/components/shared/StatsCard";
+import { Link } from "react-router-dom";
 export const description = "A line chart with dots";
+import pdf from "@/assets/[CITYPNG.COM]PDF File Document Red Icon - 1000x1000 1.svg"
+import jsx from "@/assets/image 1837.svg"
+import sql from "@/assets/image 1838.svg"
 const chartData = [
   { month: "January", desktop: 186, mobile: 80 },
   { month: "February", desktop: 305, mobile: 200 },
@@ -45,23 +52,36 @@ const statistics = [
   {
     title: "Pending Tasks",
     value: "32",
+    progressValue: "32/94",
     subValue: "+9% from last month",
     icon: <ClipboardClock />,
+    color: "bg-[#FA9E00]",
+    backgroundColor: "bg-[#FFF5E6]",
+    iconColor:"#FA9E00"
   },
   {
     title: "In Progress",
     value: "40",
+    progressValue: "40/94",
     subValue: "+12% from last month",
     icon: <ClipboardList />,
+    color: "bg-[#005AFB]",
+    backgroundColor: "bg-[#E6EFFF]",
+    iconColor:"#005AFB"
   },
   {
     title: "Completed",
     value: "32",
+    progressValue: "22/94",
     subValue: "+16% from last month",
     icon: <ClipboardCheck />,
+    color: "bg-[#2BA52E]",
+    backgroundColor: "bg-[#EAF6EA]",
+    iconColor:"#2BA52E"
   },
 ];
 export default function DashboardPage() {
+
   return (
     <div className="container mx-auto my-6">
       <div className="flex justify-center flex-wrap gap-4 p-2">
@@ -71,7 +91,12 @@ export default function DashboardPage() {
             value={item.value}
             subValue={item.subValue}
             icon={item.icon}
+            color={item.color}
+            backgroundColor={item.backgroundColor}
+            progressValue={item.progressValue}
+            iconColor={item.iconColor}
           />
+
         ))}
 
         <div className="flex justify-between p-4 shadow-sm text-black  space-y-2 rounded-sm">
@@ -91,9 +116,9 @@ export default function DashboardPage() {
         </div>
       </div>{" "}
       {/* First Row */}
-      <div className="grid grid-cols-4 ">
-        <div className="col-span-2  ">
-          <Card className="h-96 p-5">
+      <div className="flex justify-center   m-auto mt-5 w-[93%] space-x-4">
+        <div className="w-[53.5%]  max-h-60">
+          <Card className=" p-5">
             <CardHeader>
               <CardTitle>Line Chart - Dots</CardTitle>
               <CardDescription>January - June 2024</CardDescription>
@@ -150,21 +175,83 @@ export default function DashboardPage() {
           </Card>
         </div>
 
-        <div className="Second">
-          <div className="flex justify-around">
+        <div className="Second space-y-4 w-[26.5%] bg-white max-h-60 rounded-lg p-5 shadow-sm ">
+          <div className="flex justify-between">
             <h2>Active Projects</h2>
-            <a>See All</a>
+            <Link to="/"  className="text-blue-500">See All</Link>
+          </div>
+          <div className=" rounded-sm h-1/6 my-4 ">
+            <div className="flex space-x-0.5">
+              <Backpack />
+              <h2>Alpha</h2>
+            </div>
+            <div className="w-full bg-[#F2EAF2] rounded-full h-2">
+              <div className="bg-purple-900 h-2 rounded-full w-1/2 mt-3" ></div>
+              <h2>Task: 100/140</h2>
+            </div>
+          </div>
+
+          <div className="rounded-sm h-1/6 py-6">
+            <div className="flex space-x-0.5 ">
+              <Backpack />
+              <h2>SepetGo</h2>
+            </div>
+            <div className={`w-full bg-[#E6F8F7] rounded-full h-2`}>
+              <div className={`bg-[#04B5AE] h-2 rounded-full w-5/12 mt-3`} ></div>
+              <h2>Task: 52/120</h2>
+            </div>
           </div>
         </div>
 
-        <div className="third">
-          <div className="flex justify-around">
+
+        <div className="third w-[20%]  space-y-4 rounded-lg  max-h-60  bg-white py-5 px-3 shadow-sm" >
+          <div className="flex justify-between">
             <h2>Recent files</h2>
-            <p>See all</p>
+            <Link to="/"  className="text-blue-500">See All</Link>
           </div>
+           
+
+           <div className="flex justify-between  items-center space-x-1.5 pdf border-b-gray-600">
+               <div>
+                    <img src={pdf}/>
+               </div>
+               <div className="text-xs">
+                  <h2>UX_Research_Summary.pdf</h2>
+                  <p>By Mohanad Wahib • 13:00</p>
+               </div>
+               <div>
+                 <Download />
+               </div>
+           </div>
+             <div className="flex justify-around items-center space-x-1.5">
+               <div>
+                    <img src={jsx}/>
+               </div>
+               <div className="text-xs">
+                  <h2>Dashboard_Layout_React.jsx</h2>
+                  <p>By Wahib salem • 14:06</p>
+               </div>
+               <div>
+                 <Download />
+               </div>
+           </div>
+             <div className="flex justify-between items-center space-x-1.5">
+               <div>
+                    <img src={sql}/>
+               </div>
+               <div className="text-xs">
+                  <h2>Database_Schema_v2.sql</h2>
+                  <p>By Mohamed Nadir • 13:00</p>
+               </div>
+               <div>
+                 <Download />
+               </div>
+           </div>      
         </div>
       </div>
       {/* second row */}
+      
+
     </div>
-  );
-}
+  
+  )};
